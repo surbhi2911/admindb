3b3d1b42e833581da256f65f7e8335ac4b7c81b4
surbhi <surbhipansuriya633@gmail.com>
2024-12-10 12:05:25 +0530

    change mongo string

--- MongoDB/MongoConnection.js
+++ MongoDB/MongoConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const MongoDB = async () => {
   await mongoose
     .connect(
-      "mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/solarDB"
+      "mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/?retryWrites=true&w=majority&appName=solardb"
     )
     .then(() => console.log("Connected!"));
 };
