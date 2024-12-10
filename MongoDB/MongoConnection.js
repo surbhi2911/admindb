f5c5acef09d1739028dcb4db7602dfbddf643d7b
surbhi <surbhipansuriya633@gmail.com>
2024-12-10 12:15:09 +0530

    remove async await funtion

--- MongoDB/MongoConnection.js
+++ MongoDB/MongoConnection.js
@@ -1,16 +1,12 @@
 import mongoose from "mongoose";
 
-const MongoDB = async () => {
-  try {
-    await mongoose.connect("mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/?retryWrites=true&w=majority&appName=solardb", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to MongoDB!");
-  } catch (error) {
-    console.error("Failed to connect to MongoDB:", error.message);
-    process.exit(1); // Exit the process with failure
-  }
+const MongoDB = () => {
+  mongoose.connect("mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/?retryWrites=true&w=majority&appName=solardb", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("Connected to MongoDB!");
+
 };
 
 export default MongoDB;
