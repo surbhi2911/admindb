4146c7207c6e429a48de580183f409774f536a32
surbhi <surbhipansuriya633@gmail.com>
2024-12-10 12:10:16 +0530

    change mongo connection

--- MongoDB/MongoConnection.js
+++ MongoDB/MongoConnection.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 const MongoDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/?retryWrites=true&w=majority&appName=solardb"
-    )
-    .then(() => console.log("Connected!"));
+  try {
+    await mongoose.connect("mongodb+srv://surbhipansuriya772:nfaqgq0qu2dbEtmp@solardb.3lyp9.mongodb.net/?retryWrites=true&w=majority&appName=solardb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("Failed to connect to MongoDB:", error.message);
+    process.exit(1); // Exit the process with failure
+  }
 };
 
 export default MongoDB;
